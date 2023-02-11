a3cc872bd2b2e1cde60c14536549f4b714252c95
ronsh138 <88328573+ronsh138@users.noreply.github.com>
2023-02-11 13:01:39 +0200

    Update project.asm

--- project.asm
+++ project.asm
@@ -23,6 +23,10 @@ sloopcounter dw 0000h
 
 compering db 0h
 starting_text_and_keybindes db "Hello!, and welcome to the T-REX RUNNER(cursed adition).",10,10,"In this game you are a dinasour(T-REX) that runs in a field full of obstacles that you must avoid to survive.",10,10,"every 100 points the speed of the dinasour will increase so be carefull.",10,10,"to jump you simply press the space button.",10,10,10,"----to start please press space----$"
+
+up_down_count db 0
+down db 0
+up db 1
 CODESEG
 Clock  equ  40:6Ch
 
@@ -42,14 +46,24 @@ proc starting_message
 	pop dx
 	ret
 endp starting_message
-proc space_detecting
+
+proc key_detecting
 	push ax
-	mov ah, 1
-	int 21h
+	mov ah, 00h
+    int 16h
 	mov [compering], al
 	pop ax
 	ret
-endp space_detecting
+endp key_detecting
+
+proc key_pressed
+	push ax
+	mov ah, 01h
+    int 16h
+	pop ax
+	ret
+endp key_pressed
+
 proc screen_set
 	push bp
 	mov bp, sp
@@ -70,12 +84,12 @@ screen_draw:
 		mov ah, 0Ch
 		mov bh, 0
 		cmp dx, 190
-		jg background
-	land:
+		jg land
+	background:
 		mov al, 100
 		int 10h
 		jmp repeatloop
-	background:
+	land:
 		mov al, 255
 		int 10h
 	repeatloop:
@@ -94,25 +108,6 @@ screen_draw:
 	pop bp
 	ret
 endp screen_set
-;proc pixel_painter
-;	push bp
-;	mov bp, sp
-;	push ax;+1
-;	push bx;+4
-;	push cx;+6
-;	push dx;+8
-;	mov cx, [bp+6]
-;	mov dx, [bp+8]
-;	mov ax, [bp+2]
-;	mov bx, [bp+4]
-;	int 10h
-;	pop dx
-;	pop cx
-;	pop bx
-;	pop ax
-;	pop bp
-;	ret
-;endp pixel_painter
 proc dinsor
 	push bp
 	mov bp, sp
@@ -121,11 +116,11 @@ proc dinsor
 	push cx;+6
 	push dx;+8
 	push si
-	mov cx, 17h;17
-paint:
+	mov cx, 17;17
+paint1:
 	mov [bloopcountert], cx
 	mov cx, [T_REX_x]
-y_paint:
+y_paint1:
 	mov bh, 0h
 	mov dx, [T_REX_y]
 	mov si, 0
@@ -133,11 +128,11 @@ y_paint:
 	int 10h
 	dec [T_REX_y]
 	cmp [T_REX_y], 150
-	jg y_paint
+	jg y_paint1
 	inc [T_REX_x]
 	mov [T_REX_y], 190
 	mov cx, [bloopcountert]
-	loop paint
+	loop paint1
 	mov ax, [T_REX_x0]
 	mov [T_REX_x], ax
 	mov ax, [T_REX_y0]
@@ -200,9 +195,17 @@ screen:
 	mov ax, 13h
 	int 10h
 	call screen_set
+	call dinsor
+	call obstacle
 	pop ax
 	ret
 endp start_game
+proc con_game
+	call screen_set
+	call dinsor
+	call obstacle
+	ret
+endp con_game
 start:
 	mov ax, @data
 	mov ds, ax
@@ -210,41 +213,65 @@ start:
     int 10h
 	call starting_message
 wait_for_space:
-	call space_detecting
+	call key_detecting
 	cmp [compering], 20h
 	je startg
 	jmp wait_for_space
-startg:	call dinsor
-	call obstacle
-mov_obstacle:
-	dec [obstacle1_x]
-	call obstacle
-	mov cx, 6
-	call space_detecting
+startg:	
+	call start_game
+gameloop:
+	mov ah, 2Ch ; Loop system: checks every iteration if time has passed
+    int 21h ; ch=hour cl=minute dh=second dl=1/100 second
+    cmp dl, [prev_time]
+    je gameloop
+	mov [prev_time], dl
+    call key_pressed ; If no key was pressed, skip input stage
+    jz mov_obstacle
+	call key_detecting
+	cmp [compering], 1bh
+	je exit1
 	cmp [compering], 20h
-	je up
+	je jump
 	jmp mov_obstacle
-up:
+jump:
+	cmp [up], 1
+	je up1
+	cmp [down], 1
+	je down1
+up1:
 	dec [T_REX_y]
 	dec [T_REX_y0]
-	call screen_set
-	call dinsor
 	dec [obstacle1_x]
-	call obstacle
-	loop up
-	mov cx, 6
-down:
+	call con_game
+	inc [up_down_count]
+	cmp [up_down_count], 10
+	je go_down
+	jmp gameloop
+exit1:
+	jmp exit
+go_down:
+	mov [up_down_count], 0
+	mov [down], 1
+	mov [up], 0
+	jmp gameloop
+down1:
 	inc [T_REX_y]
 	inc [T_REX_y0]
-	call screen_set
-	call dinsor
 	dec [obstacle1_x]
-	call obstacle
-	loop down
-	call space_detecting
-	cmp [compering], 107
-	je exit
-	jmp mov_obstacle
+	call con_game
+	inc [up_down_count]
+	cmp [up_down_count], 10
+	je go_back_to_normal
+	jmp gameloop
+mov_obstacle:
+	dec [obstacle1_x]
+	call con_game
+	jmp gameloop
+go_back_to_normal:
+	mov [up_down_count], 0
+	mov [up], 1
+	mov [down], 0
+	jmp gameloop
 exit:
 	mov ax, 4c00h
 	int 21h
